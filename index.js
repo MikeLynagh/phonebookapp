52c9a077673f99e9727ca097650f689bda99125a
MikeLynagh <mlynagh111@gmail.com>
2023-05-16 13:45:54 -0700

    updated phonebook list

--- index.js
+++ index.js
@@ -13,24 +13,29 @@ let persons =
     [
         { 
           "id": 1,
-          "name": "Arto Hellas", 
-          "number": "040-123456"
+          "name": "Jack Black", 
+          "number": "087-1234567"
         },
         { 
           "id": 2,
-          "name": "Ada Lovelace", 
-          "number": "39-44-5323523"
+          "name": "John Doe", 
+          "number": "087 5323523"
         },
         { 
           "id": 3,
-          "name": "Dan Abramov", 
-          "number": "12-43-234345"
+          "name": "Mary Doe", 
+          "number": "086 456 4564"
         },
         { 
           "id": 4,
-          "name": "Mary Poppendieck", 
-          "number": "39-23-6423122"
-        }
+          "name": "Mary Black", 
+          "number": "087 6423122"
+        },
+        { 
+            "id": 5,
+            "name": "Harry Adwood", 
+            "number": "087 6424545"
+          },
     ]
 
 app.get("/", (request, response) => {
