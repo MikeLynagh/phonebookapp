978ea717f111e1765546febcadfed51ca2639123
MikeLynagh <mlynagh111@gmail.com>
2023-06-24 21:13:05 +0100

    new build file

--- models/person.js
+++ models/person.js
@@ -1,10 +1,11 @@
+const { urlencoded } = require("express")
 const mongoose = require("mongoose")
 
 mongoose.set("strictQuery", false)
 
 const url = process.env.MONGODB_URI
 
-console.log("connecting to", url)
+console.log("connecting to", urlencoded)
 
 mongoose.connect(url)
     .then(result => {
